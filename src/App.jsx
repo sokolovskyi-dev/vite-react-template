a71009e33fa6f96791e86e265e5ea35644ae354a
DevMasterX <sokol080288@gmail.com>
2025-11-16 23:27:25 +0200

    style: update UI component styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
+    <main className="section container">
+      <div className="flex-center">
         <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -17,15 +17,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="grid-center">
+        <h1 className="text-center">Vite + React + SWC + ESLint + Prettier + alias @</h1>
+        <div className="card grid-center">
+          <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </main>
   );
 }
 
