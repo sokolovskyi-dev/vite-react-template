6fad7d852eb3f1cb77104b13495baf4cd6095be7
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2025-12-17 08:40:49 +0200

    App.jsx, main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 
+import './App.css';
+
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
